2250e50e040689a48c7b8db48d6dbf6ac405365e
Joshua Larkin <jlarkin@microsoft.com>
2021-01-05 17:15:55 -0800

    update app to use component

--- App/App/main.cpp
+++ App/App/main.cpp
@@ -2,10 +2,13 @@
 
 using namespace winrt;
 using namespace Windows::Foundation;
+using namespace Coords;
 
 int main()
 {
     init_apartment();
-    Uri uri(L"http://aka.ms/cppwinrt");
-    printf("Hello, %ls!\n", uri.AbsoluteUri().c_str());
+    Coord a = Coord();
+    Coord b = Coord(39.0, 80.0);
+    printf("We expect the distance between %ls and %ls to be 89.0\n", a.ToString().c_str(), b.ToString().c_str());
+    printf("Calculated distance: %f", a.Distance(b));
 }
