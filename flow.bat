d72e74db592a4da5200c3f3c3d7dda6063a275d8
Joshua Larkin <jlarkin@microsoft.com>
2021-01-08 18:21:30 -0800

    everything copying except winrt.host

--- flow.bat
+++ flow.bat
@@ -4,13 +4,9 @@ echo ************************************
 
 :: This program, buildcoords, takes a version number
 set versionNum=%1
-:: Whether or not to show the binlog after building Coords
-set binlogCoords=%2
-:: Whether or not to show the binlog after building App
-set binlogApp=%3
 
 :: Display the version we want to package
-echo * Okay, you want to use version number: %versionNum% 
+echo * Okay, you want to make and use this package: Coords.2.4.%versionNum%.nupkg
 echo ------------------------------------
 
 :: msbuild Coords.csproj
@@ -22,28 +18,20 @@ echo ------------------------------------
 
 :: Add coords nupkg to feed
 echo * Adding the generated package to our local nuget feed
-start /wait "'upload' coords library" cmd /c nuget add Coords\Coords\bin\x64\debug\Coords.2.0.%versionNum%.nupkg -Source feed
+start /wait "'upload' coords library" cmd /c nuget add Coords\Coords\bin\x64\debug\Coords.2.4.%versionNum%.nupkg -Source feed
 if %ERRORLEVEL% GEQ 1 ECHO ** Error uploading package! Did you make sure the version number in Coords.csproj matches the one you input here?
 if %ERRORLEVEL% GEQ 1 EXIT /b 2
 echo ------------------------------------
 
-:: Show Coords's binlog ? 
-if %binlogCoords% GEQ 1 echo * Opening the binlog for Coords :)
-if %binlogCoords% GEQ 1 start "open the binlog" Coords\Coords\msbuild.binlog
-
 :: Update to new version 
 echo * Updating the Coords package in the App
-start "update nuget package referenced in App.sln" cmd /c nuget update App/App.sln
+start /wait "update nuget package referenced in App.sln" cmd /c nuget update App/App.sln
 echo ------------------------------------
 
 :: Build the App
 echo * Building the console App 
-start /wait "build console App" cmd /c msbuild App\App\App.vcxproj /p:Platform=x64 /bl
+start "build console App" cmd /k msbuild App\App\App.vcxproj /p:Platform=x64 /bl
 echo ------------------------------------
-
-:: Show App's binlog ?
-if %binlogApp% GEQ 1 echo * Opening the binlog for App :)
-if %binlogApp% GEQ 1 start "open the binlog" App\App\msbuild.binlog
 
 echo * Done! Halting properly...
 echo ************************************
