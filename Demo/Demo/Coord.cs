30a4c82b3bca0774f4dfa15b48133a3291338bf8
Joshua Larkin <jlarkin@microsoft.com>
2021-01-12 14:38:44 -0800

    soln

---
+++ Demo/Demo/Coord.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Demo
+{
+    public sealed class Coord
+    {
+        public long X;
+        public long Y;
+
+        public Coord()
+        {
+            X = 0L;
+            Y = 0L;
+        }
+
+        public Coord(long x, long y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double Distance(Coord target)
+        {
+            double deltaX = X - target.X;
+            double deltaY = Y - target.Y;
+            return Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+        }
+
+        public override string ToString()
+        {
+            return "(" + X + "," + Y + ")";
+        }
+    }
+}
