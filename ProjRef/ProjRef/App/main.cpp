bed9940a8b4f8b726ae03099b123477a3726c7a8
Joshua Larkin <jlarkin@microsoft.com>
2021-02-04 10:30:43 -0800

    add project ref sln

---
+++ ProjRef/ProjRef/App/main.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace Coords;
+
+int main()
+{
+    init_apartment();
+    printf("Starting up!\n");
+    Coord a = Coord();
+    Coord b = Coord(39.0, 80.0);
+    printf("We expect the distance between %ls and %ls to be 89.0\n", a.ToString().c_str(), b.ToString().c_str());
+    printf("Calculated distance: %f", a.Distance(b));
+}
---
+++ ProjRef/ProjRef/CsApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Coords;
+
+namespace CsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Coord a = new Coord();
+            Coord b = new Coord(39.0, 80.0);
+            Console.WriteLine("Live from C# it is coords " + a.ToString() + " and " + b.ToString());
+            Console.WriteLine("The distance was " + a.Distance(b));
+        }
+    }
+}
