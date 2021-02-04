62f0d605050b40993e811f3d908499ac0bab5eb5
Joshua Larkin <jlarkin@microsoft.com>
2021-02-04 10:38:13 -0800

    update targets

--- update-targets.bat
+++ update-targets.bat
@@ -1,31 +1,27 @@
 @echo off
 
 echo ************************************
-echo [Start] Copy the CsWinRT.Authoring.targets file and CsWinRT.targets to the "cswinrt 1.1.1-prerelease.210109.2" nuget folder
+echo [Start] Copy targets files to the "cswinrt 1.1.2-prerelease.210130.1" nuget folder
 echo ************************************
 
 
 set cswinrtVersion=
 set sourceDir=c:\gh\HostSupport\CsWinRT_Targets
-set destinationBuild=c:\users\jlarkin\.nuget\packages\microsoft.windows.cswinrt\1.1.1-prerelease.210109.2\build
-set destinationBuildTransitive=c:\users\jlarkin\.nuget\packages\microsoft.windows.cswinrt\1.1.1-prerelease.210109.2\buildTransitive
+set destinationBuild=c:\users\jlarkin\.nuget\packages\microsoft.windows.cswinrt\1.1.2-prerelease.210130.1\build
+set destinationBuildTransitive=c:\users\jlarkin\.nuget\packages\microsoft.windows.cswinrt\1.1.2-prerelease.210130.1\buildTransitive
 set plainTargets=Microsoft.Windows.CsWinRT.targets
 set authoringTargets=Microsoft.Windows.CsWinRT.Authoring.targets
-
-:: we need authoring and plain targets in build
-:: we need authoring in buildTransitive , wait do we even need buildtransitive?
-::if its a pakcagerefernece, its a c# app consuming and wont need any native stuff
-:: can c++ consumers use packagereference? 
+set authoringTransitiveTargets=Microsoft.Windows.CsWinRT.Authoring.Transitive.targets
 
 echo **********************************
 echo * update the nuget\build directory
 echo **********************************
-robocopy %sourceDir% %destinationBuild% %plainTargets% %authoringTargets%
+robocopy %sourceDir% %destinationBuild% %plainTargets% %authoringTargets% %authoringTransitiveTargets%
 
 echo ********************************************
 echo * update the nuget\buildTransitive directory
 echo ********************************************
-robocopy %sourceDir% %destinationBuildTransitive% %authoringTargets%
+robocopy %sourceDir% %destinationBuildTransitive% %authoringTransitiveTargets%
 
 
 echo ************************************
